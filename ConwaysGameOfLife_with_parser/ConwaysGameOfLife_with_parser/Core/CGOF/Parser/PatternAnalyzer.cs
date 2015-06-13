3a1526af2f0deedd145e654d1afbc45291b8267e
NET-D3v3l0p3r <3r0rxx@gmail.com>
2015-06-14 00:55:27 +0200

    Update PatternAnalyzer.cs

--- ConwaysGameOfLife_with_parser/ConwaysGameOfLife_with_parser/Core/CGOF/Parser/PatternAnalyzer.cs
+++ ConwaysGameOfLife_with_parser/ConwaysGameOfLife_with_parser/Core/CGOF/Parser/PatternAnalyzer.cs
@@ -40,7 +40,7 @@ namespace ConwaysGameOfLife_with_parser.Core.Parser
             var colors = ColorTools.getAndSortBrightestPixel(Pattern);
 
             if (colors.Count > 3)
-                throw new Exception("Pattern is not supported ...(yet.. update 0.2 will fix it.. biiznillah!)");
+                throw new Exception("This kind of pattern is not supported ...(yet.. update 0.02 will fix it.. biiznillah!)");
 
             DeadCell = colors[colors.Count - 1];
             Stripe = colors[colors.Count - 2];
